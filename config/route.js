3380b60db31758abd5268d2e9f7cf527191e89fa
Rauf Guven <mrguven@gmail.com>
2023-06-02 16:15:18 +0200

    update

--- config/route.js
+++ config/route.js
@@ -1,20 +1,23 @@
 const express= require('express');
 const router = express.Router();
 
-const {getHomePage,getFeedPage,sendComment,getFullMessage,
-    deleteUser,getEditeditMessage}= require('../controller/controller.js')
+const {getHomePage,getFeedPage,sendMessage,getFullMessage,
+    deleteUser,getEditeditMessage,updateMessage}= require('../controller/controller.js')
 
 
 router.get('/', getHomePage);
 
 router.get('/feed',getFeedPage)
 
-router.post('/sendComment', sendComment);
+router.post('/sendMessage', sendMessage);
 router.get('/fullMessage/:id', getFullMessage);
 
 router.get('/editMessage/:id', getEditeditMessage);
 
-router.post('/deleteUser/:id', deleteUser)
+router.post('/deleteUser/:id', deleteUser);
+
+router.post('/updateMessage/:id', updateMessage)
+
 
 
 module.exports= router
--- controller/controller.js
+++ controller/controller.js
@@ -10,31 +10,31 @@ const getHomePage =(req,res) => {
 
 }
 
+const sendMessage = (req,res)=> {
 
+   
+        
+    const message= new FeedModel(req.body
+            )
+        console.log(req.body.Name);
+        console.log(req.body.Message);
+        message.save().then(()=>{res.status(200).redirect('/feed')})
+             .catch((err1)=> {res.render('wrongAddCommentPage', {err1: err1.errors})});
+           
+           
+            
+            console.log('did this function run');
+     }
 
 
 const getFeedPage = (req,res) =>{
     FeedModel.find().sort({ createdAt: -1 })
-                            .then((data)=> {res.render('feed', {blog: data})}) 
+                            .then((data)=> {res.render('feed', {message: data})}) 
                             .catch(()=>res.render('404'))
 }
 
 
-const sendComment = (req,res)=> {
 
-   
-        
-            const message= new FeedModel(req.body
-                    )
-                console.log(req.body.Name);
-                console.log(req.body.Message);
-                      blog.save().then(()=>{res.status(200).redirect('/feed')})
-                     .catch((err1)=> {res.render('wrongAddCommentPage', {err1: err1.errors})});
-                   
-                   
-                    
-                    console.log('did this function run');
-             }
 
 
              const getFullMessage = (req,res)=> {
@@ -49,7 +49,7 @@ const sendComment = (req,res)=> {
     const getEditeditMessage = (req,res)=> {
     
         FeedModel.findById(req.params.id).then((result)=> {
-            res.render('editedMessage', {blog:result})
+            res.render('editedMessage', {message:result})
         }).catch((err)=>{
             console.log(err);
         })
@@ -65,5 +65,13 @@ const sendComment = (req,res)=> {
     }
 
 
+    const updateMessage=(req,res)=> {
+    
+        FeedModel.findByIdAndUpdate(req.params.id,req.body,{new: true})
+        .then((result)=> res.render('feed', {message:result})).catch(()=> res.render('404'))
+        
+    
+    }
+
 module.exports={
-    getHomePage,getFeedPage,sendComment,getFullMessage,getEditeditMessage,deleteUser}
+    getHomePage,getFeedPage,sendMessage,getFullMessage,getEditeditMessage,deleteUser,updateMessage}
